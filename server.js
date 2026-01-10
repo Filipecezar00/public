276494364426e72110236fe8e50b43f8b3e06d46
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-09 21:53:20 -0300

    atualizando

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const {MongoClient} = require('mongodb');
 const express = require('express'); 
 const path = require("path"); 
 
+const app = express()
+const url = process.env.MONGO_URL 
+const client = new MongoClient(url)
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true})) 
+
+async function iniciarServidor(){
+    
+}
